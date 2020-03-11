3615f91d09c75ccaaea62641b224ecc93aaccb22
kennja05 <jacobkenny05@gmail.com>
2020-03-11 18:49:20 -0400

    added note to routes file for limiting unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,3 +16,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# resources :userhome, :only => [:index] - method to limit routes that won't be needed.
