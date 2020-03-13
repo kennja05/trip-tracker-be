89fb5fdcd714ae2a848b97376a1ebbfc8c2ddbd3
kennja05 <jacobkenny05@gmail.com>
2020-03-12 23:58:28 -0400

    added show method for user class

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,9 @@ class Api::V1::UsersController < ApplicationController
         render json: users
     end 
 
+    def show
+        user = User.find(params['id'])
+        render json: user
+    end 
+
 end
