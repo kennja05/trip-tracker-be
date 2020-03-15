c534fb9b69697bb376f6510f771d25c2c056d276
kennja05 <jacobkenny05@gmail.com>
2020-03-15 16:24:56 -0400

    wrote seed data creator for Currency. had to remove South Sudan as an available country

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'dotenv'
 User.destroy_all
 Destination.destroy_all
 Value.destroy_all
+Currency.destroy_all
 
 user1 = User.create(name: 'Jacob', phone: '5707161763', username: 'kennja05', email: 'jacobkenny05@gmail.com', password: '123', image: 'https://humanorigins.si.edu/sites/default/files/styles/full_width/public/images/square/neanderthalensis_JG_Recon_Head_CC_3qtr_lt_sq.jpg?itok=65pnoWxu')
 user2 = User.create(name: 'Megan', phone: '9087701138', username: 'mcdomeg24', email: 'meganmcdonald24@gmail.com', password: '123', image: '')
@@ -20,20 +21,21 @@ end
 
 # Creating destination to match with currency codes. I have selected the most-relevant currecies for destinations with multiple options 
 destinations = JSON.parse(RestClient.get('https://restcountries.eu/rest/v2/all'))
-destinations.each do |country| 
-    if country['name'] === "Antarctica" || country['name'] === "Virgin Islands (British)" || country['name'] === "Micronesia (Federated States of)" || country['name'] === "Palau" || country['name'] === "Singapore"
-        Destination.create(name: country['name'], native_name: country['nativeName'], capital: country['capital'], code: country['currencies'][1]['code'], symbol: country['currencies'][1]['symbol'], currency_name: country['currencies'][1]['name'])
-    elsif country['name'] === "Zimbabwe"
-        Destination.create(name: country['name'], native_name: country['nativeName'], capital: country['capital'], code: country['currencies'][7]['code'], symbol: country['currencies'][7]['symbol'], currency_name: country['currencies'][7]['name'])
-    else
-        Destination.create(name: country['name'], native_name: country['nativeName'], capital: country['capital'], code: country['currencies'][0]['code'], symbol: country['currencies'][0]['symbol'], currency_name: country['currencies'][0]['name'])    
-    end
+destinations.each do |country|
+    if country['name'] != "South Sudan" #The ER tracker does not have the South Sudan Pound Available, and the Countries API lists no alternate Currencies
+        if country['name'] === "Antarctica" || country['name'] === "Virgin Islands (British)" || country['name'] === "Micronesia (Federated States of)" || country['name'] === "Palau" || country['name'] === "Singapore"
+            Destination.create(image: country['flag'], name: country['name'], native_name: country['nativeName'], capital: country['capital'], code: country['currencies'][1]['code'], symbol: country['currencies'][1]['symbol'], currency_name: country['currencies'][1]['name'])
+        elsif country['name'] === "Zimbabwe"
+            Destination.create(image: country['flag'], name: country['name'], native_name: country['nativeName'], capital: country['capital'], code: country['currencies'][7]['code'], symbol: country['currencies'][7]['symbol'], currency_name: country['currencies'][7]['name'])
+        else
+            Destination.create(image: country['flag'], name: country['name'], native_name: country['nativeName'], capital: country['capital'], code: country['currencies'][0]['code'], symbol: country['currencies'][0]['symbol'], currency_name: country['currencies'][0]['name'])    
+        end
+    end 
 end 
 
 #Creating Currencies. The purpose of this is to allow for the destination to always have access to the most recent ER
 
 Destination.all.each do |destination| 
-    valId = Value.all.select {|value| value.code === destination.code}
-    myCurrency = Currency.create(destination_id: destination.id, value_id: valId)
-    
+    matchingValue = Value.find_by(code: destination.code)
+    myCurrency = Currency.create(destination_id: destination.id, value_id: matchingValue.id) 
 end 
