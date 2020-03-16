88d040953936285447608fafd92ff17e20ff34d2
kennja05 <jacobkenny05@gmail.com>
2020-03-16 14:31:01 -0400

    added index and show methods for PE controller

--- app/controllers/api/v1/planned_expenses_controller.rb
+++ app/controllers/api/v1/planned_expenses_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::PlannedExpensesController < ApplicationController
+
+    def index
+        plannedExpenses = PlannedExpense.all 
+        render json: plannedExpenses
+    end 
+
+    def show
+        plannedExpense = PlannedExpense.find(params['id'])
+        render json: plannedExpense 
+    end 
+
+
 end
