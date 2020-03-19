434bd26743dd126db293636c5409f00a701b2e4d
kennja05 <jacobkenny05@gmail.com>
2020-03-18 22:41:50 -0400

    added default name for plannedexpense

--- app/models/planned_expense.rb
+++ app/models/planned_expense.rb
@@ -2,4 +2,13 @@ class PlannedExpense < ApplicationRecord
     belongs_to :trip
 
     validates :cost, numericality: { greater_than: 0}
+    before_save :defaultName
+
+    def defaultName
+        if self.name === ""
+            self.name = self.category
+        end
+    end 
+
+
 end
