e96955d244e5a558ce5b78e00588bf4193ad78d7
kennja05 <jacobkenny05@gmail.com>
2020-03-23 15:29:40 -0400

    added destroy dependent to trip for plannedExpenses

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ApplicationRecord
 
     belongs_to :user
     belongs_to :destination
-    has_many :planned_expenses
     has_many :values, through: :destination
+    has_many :planned_expenses, dependent: :destroy
 
 end
