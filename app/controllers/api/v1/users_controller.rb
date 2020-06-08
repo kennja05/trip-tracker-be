a9d4db3d19b7ad154432db04752281be2dd8dab9
kennja05 <jacobkenny05@gmail.com>
2020-06-07 21:14:32 -0400

    basic version of signup validation in place

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::UsersController < ApplicationController
     def login
         user = User.find_by(username: params['username'])
         if !user
-            render json: null
+            render json: nil
         else
             if user.password_digest === (params['password_digest'])
                 render json: user
@@ -18,14 +18,6 @@ class Api::V1::UsersController < ApplicationController
         end
     end 
 
-    # def login
-    #     @user = User.find_by_email(params[:email])
-    #     if @user.password == params[:password]
-    #       give_token
-    #     else
-    #       redirect_to home_url
-    #     end
-    # end
 
     def show
         user = User.find(params['id'])
@@ -34,10 +26,11 @@ class Api::V1::UsersController < ApplicationController
  
     def create 
         user = User.new(userParams)
+        byebug
         if user.save
             render json: user
         else  
-            render json: {errors: user.errors.full_messages} 
+            render json: {'errors' => user.errors.full_messages} 
         end
     end
 
