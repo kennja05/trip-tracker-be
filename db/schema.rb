770682ed34fcc4001459d2947559ed002251028c
kennja05 <jacobkenny05@gmail.com>
2020-06-08 22:30:51 -0400

    attempted to correct schema with password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_200639) do
+ActiveRecord::Schema.define(version: 2020_06_09_022214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,10 +55,10 @@ ActiveRecord::Schema.define(version: 2020_06_06_200639) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "values", force: :cascade do |t|
