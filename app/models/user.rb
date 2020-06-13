d481ccebfbf6afc09a27ab9dfc5408726e78e760
kennja05 <jacobkenny05@gmail.com>
2020-06-13 17:04:01 -0400

    removed commented out code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,6 @@ class User < ApplicationRecord
     has_many :trips
     has_many :destinations, through: :trips
     validates :username, uniqueness: { case_sensitive: false }, length: {minimum: 5} 
-    # validate :password_must_be_strong
     
-
-    # def password_must_be_strong
-    #     if password_digest.length < 3
-    #         errors.add(:password, 'must be longer than 3 characters')
-    #     end
-    # end
 
 end
