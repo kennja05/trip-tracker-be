cbfd7f9f26b62cd7af82199a6cb4777f3d10a7aa
kennja05 <jacobkenny05@gmail.com>
2020-06-30 12:28:17 -0400

    add procfile. update env key

--- app/models/rate_getter.rb
+++ app/models/rate_getter.rb
@@ -1,12 +1,13 @@
-require 'dotenv'
+# require 'dotenv'
 require 'rest-client'
 
 class RateGetter < ApplicationRecord
 
-    @@API_KEY = ENV['exchangeRateApiKey']
+    # @@API_KEY = ENV['exchangeRateApiKey']
 
     def self.obtainNewestRates 
-        rates = JSON.parse(RestClient.get("http://data.fixer.io/api/latest?access_key=#{@@API_KEY}&base=usd"))
+        key = ENV['exchangeRateApiKey']
+        rates = JSON.parse(RestClient.get("http://data.fixer.io/api/latest?access_key=#{key}&base=usd"))
         i = 0
         while i < rates['rates'].length
             Value.create(date: rates['date'], code: rates['rates'].keys[i], rate: rates['rates'].values[i].round(3)) 
