8ae047e330a308d3cf2027e0e16d6a7b1f34032f
kennja05 <jacobkenny05@gmail.com>
2020-07-21 11:45:55 -0400

    update rates at creation method

--- app/controllers/api/v1/rates_controller.rb
+++ app/controllers/api/v1/rates_controller.rb
@@ -11,18 +11,27 @@ class Api::V1::RatesController < ApplicationController
         render json: rate
     end
 
-    def mostRecent #this will get the two most recent rate objects
+    def mostRecent #this will get the two most recent rate objects to see if the most recent exhange rate is up or down
         rates = Rate.all.last(2)
         render json: rates
     end
 
     def ratesAtCreation #this will look to get the rates at the time in which the trip was created
         formattedDate = params['date'].split('-').join('').to_i
-        rates = Rate.all.filter do |rate|
+        rates = Rate.all.filter do |rate| ##get all the rates that have the right date
             rate.rate_list['date'].split('-').join('').to_i >= formattedDate 
         end
-        byebug
-        render json: rates 
+        res = []
+        rates.each do |rate|
+            myObj = { #obtain only the necessary information from those ratesc
+                rate: rate.rate_list['rates'][params['code']],
+                date: rate.created_at
+            }
+            
+            res << myObj
+        end
+        
+        render json: res 
     end
 
 end
