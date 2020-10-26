245e4c3fd7ab5e56b752814b3d866b06f28e99ae
kennja05 <jacobkenny05@gmail.com>
2020-10-26 19:17:18 -0400

    re-attempt cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,27 +7,27 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'trip-tracker.netlify.app' 
+    origins ['trip-tracker.netlify.app', 'localhost:3001', '*']  
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
-  allow do
-    origins 'localhost:3001' 
+  # allow do
+  #   origins 'localhost:3001' 
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 
-  allow do
-    origins '*' 
+  # allow do
+  #   origins '*' 
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 
 end
