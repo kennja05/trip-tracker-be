237363aa3a22b9873321f40c251877eb3e28efb5
Jacob Kenny <jacobkenny05@gmail.com>
2022-06-16 21:34:19 -0400

    update country api key

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 # Destination.destroy_all
 key = ENV['countryApiKey']
 # Creating destination to match with currency codes. I have selected the most-relevant currecies for destinations with multiple options 
-destinations = JSON.parse(RestClient.get("https://restcountries.eu/rest/v2/all?access_key=#{key}"))
+destinations = JSON.parse(RestClient.get("https://restcountries.eu/rest/v2/all?access_key=f76989e6cc7d487793917c3faca656dd"))
 destinations.each do |country|
     if country['name'] != "South Sudan" #The ER tracker does not have the South Sudan Pound Available, and the Countries API lists no alternate Currencies
         if country['name'] === "Antarctica" || country['name'] === "Virgin Islands (British)" || country['name'] === "Micronesia (Federated States of)" || country['name'] === "Palau" || country['name'] === "Singapore"
